2633fe85e90fec96fee80da5e32a03a9e0664174
Sibaprasad Maiti <sibu.it13@gmail.com>
2023-08-15 07:07:07 +0530

    Hello world in x86_64 assembly and ada

---
+++ hello-world/x86_64/hello.asm
@@ -0,0 +1,22 @@
+
+; nasm -f macho64 hello.asm
+; ld -e start -macosx_version_min 10.13.0 -static hello.o
+
+
+
+		global		start
+
+		section		.text
+start:	mov			rax, 0x02000004
+		mov			rdi, 1
+		mov 		rsi, message
+		mov 		rdx, 13
+		syscall
+		mov 		rax, 0x02000001
+		xor			rdi, rdi
+		syscall
+
+
+		section 	.data
+message: db			"Hello World", 10
+
